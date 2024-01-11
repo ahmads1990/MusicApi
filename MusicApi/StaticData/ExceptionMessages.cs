58a6c3a3579272fcb59fc00414046cff664394f6
ahmads1990 <ahmads20002000@gmail.com>
2024-01-11 20:26:45 +0200

    Refactor ExceptionMessages InvalidEntityId message

--- MusicApi/StaticData/ExceptionMessages.cs
+++ MusicApi/StaticData/ExceptionMessages.cs
@@ -5,7 +5,7 @@
         // Message for invalid entity data, which maybe fields are null or bad format.
         public static string InvalidEntityData = "Invalid entity data, check format and value for required fields.";
         // Message for an invalid entity ID (create -> id=0, update/del -> id!=0).
-        public static string InvalidEntityId = "Invalid ID (create -> id=0, update/del -> id!=0).";
+        public static string InvalidEntityId = "Invalid ID, wanted format (create -> id=0, update/del -> id>0).";
         // Message for when an entity doesn't exist for update/delete operations.
         public static string EntityDoesntExist = "Check ID, can't find entity to update/delete.";
     }
