eb9ce7ccae5a24315e0e6c24f9cc2eadc47e2d2f
ahmads1990 <ahmads20002000@gmail.com>
2024-01-22 09:03:21 +0200

    added token creation in AuthService and used it in RegisterUserAsync

--- MusicApi/Services/AuthService.cs
+++ MusicApi/Services/AuthService.cs
@@ -1,12 +1,20 @@
 ﻿
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 namespace MusicApi.Services
 {
     public class AuthService : IAuthService
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        public AuthService(UserManager<ApplicationUser> userManager)
+        private readonly JwtConfig _jwtConfig;
+        public AuthService(UserManager<ApplicationUser> userManager, IOptions<JwtConfig> jwtConfig)
         {
             _userManager = userManager;
+            _jwtConfig = jwtConfig.Value;
         }
         public async Task<AuthModel> RegisterUserAsync(RegisterModel registerModel)
         {
@@ -33,13 +41,49 @@ namespace MusicApi.Services
                 return new AuthModel { Message = errorMessage };
             }
 
-            // user creation went ok  
+            // user creation went ok then create token and send it back
+            var jwtToken = await CreateJwtToken(user);
+
             return new AuthModel
             {
-                Email = user.Email,
                 IsAuthenticated = true,
-                Username = user.UserName
+                Username = user.UserName,
+                Email = user.Email,
+                Claims = new List<string>(),
+                Token=new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                ExpiresOn=jwtToken.ValidTo                         
             };
+        }
+        private async Task<JwtSecurityToken> CreateJwtToken(ApplicationUser user)
+        {
+            if (user is null) return null;
+            // get user claims
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            // create jwt claims
+            var jwtClaims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            };
+            // merge both claims lists to jwtClaims
+            jwtClaims = (Claim[])jwtClaims.Union(userClaims);
+
+            // specify the signing key and algorithm
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfig.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            // finally create the token
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwtConfig.Issuer,
+                audience: _jwtConfig.Audience,
+                claims: jwtClaims,
+                expires: DateTime.Now.AddHours(_jwtConfig.DurationInHours),
+                signingCredentials: signingCredentials
+                );
+
+            return jwtSecurityToken;
         }
     }
 }
