7c55f05e8e0751bff22bc56209ad7a0fa2027a84
ahmads1990 <ahmads20002000@gmail.com>
2024-01-22 08:25:53 +0200

    Added helper class jwt to map config info

--- MusicApi/Program.cs
+++ MusicApi/Program.cs
@@ -22,8 +22,10 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 // Security
+// configure jwt helper class to use jwt config info
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("Jwt"));
+// add Identity with options configuration
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>{
-    // Identity options configuration
     options.Password.RequireDigit = true;
     options.Password.RequiredLength = 8;
     options.Password.RequireNonAlphanumeric = true;
@@ -31,21 +33,24 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>{
     options.Password.RequireLowercase = true;
 }).AddEntityFrameworkStores<AppDbContext>();
 
+// Add Authentication with jwt config
 builder.Services.AddAuthentication(options =>{
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(o =>
     {
+        o.RequireHttpsMetadata = false;
+        o.SaveToken = false;
         o.TokenValidationParameters = new TokenValidationParameters
         {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
             ValidIssuer = builder.Configuration["Jwt:Issuer"],
             ValidAudience = builder.Configuration["Jwt:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = false,
-            ValidateIssuerSigningKey = true
         };
     });
 
