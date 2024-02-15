ab6ad5e1c9ec57fd1ef25e40c75c7597e586b91d
Ahmad <ahmads20002000@gmail.com>
2024-02-15 13:10:01 +0200

    Update track model with path, length, releaseDate

---
+++ MusicApi/Migrations/20240215110904_UpdateTrackWithMoreProps.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTrackWithMoreProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Length",
+                table: "Tracks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Tracks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrackPath",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "TrackPath",
+                table: "Tracks");
+        }
+    }
+}
--- MusicApi/Models/Track.cs
+++ MusicApi/Models/Track.cs
@@ -6,7 +6,10 @@ namespace MusicApi.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        // Nav prop
+        public string TrackPath { get; set; }
+        public int Length { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        // Genre refrencep
         public ICollection<Genre> Genres { get; set; }
     }
 }
