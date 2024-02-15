ab9c3a8201678e5cd9e06b10048a5fa47dc00a80
Ahmad <ahmads20002000@gmail.com>
2024-02-15 13:38:29 +0200

    Update genre model with Description

---
+++ MusicApi/Migrations/20240215113815_UpdateGenreWithDesc.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGenreWithDesc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Genres");
+        }
+    }
+}
--- MusicApi/Models/Genre.cs
+++ MusicApi/Models/Genre.cs
@@ -7,6 +7,7 @@ namespace MusicApi.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         // Nav prop
         [JsonIgnore]
         public ICollection<Track> Tracks { get; set; }
