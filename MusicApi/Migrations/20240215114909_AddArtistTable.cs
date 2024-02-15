2ad64e0e03c72057d5cb75d525d748241cf47dc2
Ahmad <ahmads20002000@gmail.com>
2024-02-15 13:49:56 +0200

    Add Artist model

---
+++ MusicApi/Migrations/20240215114909_AddArtistTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddArtistTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Length",
+                table: "Tracks",
+                newName: "LengthInSeconds");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LengthInSeconds",
+                table: "Tracks",
+                newName: "Length");
+        }
+    }
+}
---
+++ MusicApi/Models/Artist.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicApi.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Bio { get; set; }
+        public int Followers { get; set; }
+        //public string CoverPath { get; set; }
+        //Albums
+    }
+}
