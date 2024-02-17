ab9f6489861fc85e332449beeccaf4ecbc169057
ahmads1990 <ahmads20002000@gmail.com>
2024-02-17 06:40:52 +0200

    Add new Dtos AddArtistDto, ArtistDto

---
+++ MusicApi/Dtos/AddArtistDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicApi.Dtos
+{
+    public class AddArtistDto
+    {
+        public string Bio { get; set; }
+        public int Followers { get; set; }
+    }
+}
---
+++ MusicApi/Dtos/ArtistDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicApi.Dtos
+{
+    public class ArtistDto
+    {
+        public int Id { get; set; }
+        public string Bio { get; set; }
+        public int Followers { get; set; }
+    }
+}
