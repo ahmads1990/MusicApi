d371cd8bce935fb1e3ba001e94434ab8494eff05
Ahmad <ahmads20002000@gmail.com>
2024-03-14 23:52:57 +0200

    Implement ArtistService

--- MusicApi/Services/ArtistService.cs
+++ MusicApi/Services/ArtistService.cs
@@ -1,55 +1,97 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+
 namespace MusicApi.Services
 {
     public class ArtistService : IArtistService
     {
         private readonly AppDbContext _dbContext;
+
         public ArtistService(AppDbContext dbContext)
         {
             _dbContext = dbContext;
         }
-        public Task<IEnumerable<Artist>> GetAllAsync()
+
+        public async Task<IEnumerable<Artist>> GetAllAsync()
         {
-            throw new NotImplementedException();
-        }
-        public Artist GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<Artist> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<Artist> SearchByNameAsync(string Name)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<bool> CheckArtistExistAsync(int id)
-        {
-            throw new NotImplementedException();
+            return await _dbContext.Artists
+                .ToListAsync();
         }
 
-        public Task<Artist> CreateNewArtist(Artist newArtist)
+        public Artist? GetById(int id)
         {
-            throw new NotImplementedException();
+            return _dbContext.Artists
+                .FirstOrDefault(a => a.Id == id);
         }
 
-
-
-
-
-
-
-
-        public Artist UpdateArtist(Artist artist)
+        public async Task<Artist?> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
-        }
-        public Artist DeleteArtistk(Artist artist)
-        {
-            throw new NotImplementedException();
+            return await _dbContext.Artists
+                .FirstOrDefaultAsync(a => a.Id == id);
         }
 
+        public async Task<Artist?> SearchByNameAsync(string name)
+        {
+            return await _dbContext.Artists
+                .FirstOrDefaultAsync(a => a.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+        }
 
+        public async Task<bool> CheckArtistExistAsync(int id)
+        {
+            return await _dbContext.Artists
+                .AnyAsync(a => a.Id == id);
+        }
+
+        public async Task<Artist> CreateNewArtist(Artist newArtist)
+        {
+            if (newArtist == null || string.IsNullOrEmpty(newArtist.Name))
+                throw new ArgumentException(ExceptionMessages.InvalidEntityData);
+
+            if (newArtist.Id != 0)
+                throw new ArgumentException(ExceptionMessages.InvalidEntityId);
+
+            // Cover image
+            //TODO
+            var createdArtist = await _dbContext.Artists
+                .AddAsync(newArtist);
+            await _dbContext.SaveChangesAsync();
+
+            return createdArtist.Entity;
+        }
+
+        public async Task<Artist?> UpdateArtistAsync(Artist artist)
+        {
+            if (artist == null || string.IsNullOrEmpty(artist.Name))
+                throw new ArgumentException(ExceptionMessages.InvalidEntityData);
+
+            if (artist.Id <= 0)
+                throw new ArgumentException(ExceptionMessages.InvalidEntityId);
+
+            bool doesArtistExist = await CheckArtistExistAsync(artist.Id);
+            if (!doesArtistExist) return null;
+
+            var updatedArtist = _dbContext.Artists
+                .Update(artist);
+            await _dbContext.SaveChangesAsync();
+
+            return updatedArtist.Entity;
+        }
+
+        public async Task<Artist?> DeleteArtistAsync(Artist artist)
+        {
+            if (artist == null)
+                throw new ArgumentException(ExceptionMessages.InvalidEntityData);
+
+            if (artist.Id <= 0)
+                throw new ArgumentException(ExceptionMessages.InvalidEntityId);
+
+            bool doesArtistExist = await CheckArtistExistAsync(artist.Id);
+            if (!doesArtistExist) return null;
+
+            var deletedArtist = _dbContext.Artists
+                .Remove(artist);
+            await _dbContext.SaveChangesAsync();
+
+            return deletedArtist.Entity;
+        }
     }
 }
--- MusicApi/Services/Interfaces/IArtistService.cs
+++ MusicApi/Services/Interfaces/IArtistService.cs
@@ -3,12 +3,12 @@
     public interface IArtistService
     {
         public Task<IEnumerable<Artist>> GetAllAsync();
-        public Artist GetById(int id);
-        public Task<Artist> GetByIdAsync(int id);
-        public Task<Artist> SearchByNameAsync(string Name);
+        public Artist? GetById(int id);
+        public Task<Artist?> GetByIdAsync(int id);
+        public Task<Artist?> SearchByNameAsync(string Name);
         public Task<bool> CheckArtistExistAsync(int id);
         public Task<Artist> CreateNewArtist(Artist newArtist);
-        public Artist UpdateArtist(Artist artist);
-        public Artist DeleteArtistk(Artist artist);
+        public Task<Artist?> UpdateArtistAsync(Artist artist);
+        public Task<Artist?> DeleteArtistAsync(Artist artist);
     }
 }
