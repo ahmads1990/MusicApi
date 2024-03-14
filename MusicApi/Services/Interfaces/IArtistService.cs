8a14bc12de295761ce2b54ef6338472a15b6e743
Ahmad <ahmads20002000@gmail.com>
2024-03-14 23:28:46 +0200

    Add IArtistService

---
+++ MusicApi/Services/Interfaces/IArtistService.cs
@@ -0,0 +1,14 @@
+﻿namespace MusicApi.Services.Interfaces
+{
+    public interface IArtistService
+    {
+        public Task<IEnumerable<Artist>> GetAllAsync();
+        public Artist GetById(int id);
+        public Task<Artist> GetByIdAsync(int id);
+        public Task<Artist> SearchByNameAsync(string Name);
+        public Task<bool> CheckArtistExistAsync(int id);
+        public Task<Artist> CreateNewArtist(Artist newArtist);
+        public Artist UpdateArtist(Artist artist);
+        public Artist DeleteArtistk(Artist artist);
+    }
+}
