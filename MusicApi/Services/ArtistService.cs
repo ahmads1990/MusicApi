5ba52050e55a9c3bf34a45055e8bf41802d31a09
Ahmad <ahmads20002000@gmail.com>
2024-03-14 23:34:27 +0200

    Add initial ArtistService class

---
+++ MusicApi/Services/ArtistService.cs
@@ -0,0 +1,55 @@
+﻿
+namespace MusicApi.Services
+{
+    public class ArtistService : IArtistService
+    {
+        private readonly AppDbContext _dbContext;
+        public ArtistService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<IEnumerable<Artist>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Artist GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Artist> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Artist> SearchByNameAsync(string Name)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> CheckArtistExistAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Artist> CreateNewArtist(Artist newArtist)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+        public Artist UpdateArtist(Artist artist)
+        {
+            throw new NotImplementedException();
+        }
+        public Artist DeleteArtistk(Artist artist)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
